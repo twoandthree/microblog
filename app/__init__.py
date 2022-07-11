1bdfac52a3dbe28c68b992a9b45ee641afbce620
twoandthree <stevenjohnston@email.arizona.edu>
2022-07-10 23:10:46 -0700

    Added flask_login extention to __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from flask_login import LoginManager
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy 
@@ -7,5 +8,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 from app import routes, models
