dc19b5d924c416e5e5cecd2cbad6456c0f3d81e1
twoandthree <stevenjohnston@email.arizona.edu>
2022-07-21 07:39:23 -0700

    Users can now upload avatars.

--- app/models.py
+++ app/models.py
@@ -1,14 +1,23 @@
+from hashlib import md5
+from app import login
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from app import db
 
+@login.user_loader
+def load_user(id):
+  return User.query.get(int(id))
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(64), index=True, unique=True)
   email = db.Column(db.String(120), index=True, unique=True)
   password_hash = db.Column(db.String(128))
   posts = db.relationship('Post', backref='author', lazy='dynamic')
+  about_me = db.Column(db.String(140))
+  last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
 
   def __repr__(self):
@@ -19,6 +28,10 @@ class User(db.Model):
   
   def check_password(self, password):
     return check_password_hash(self.password_hash, password)
+
+  def avatar(self, size):
+    digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+    return f'http://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 
 class Post(db.Model):
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,20 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, login_required, logout_user
+from werkzeug.urls import url_parse
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
+
+
+@app.route('/logout')
+def logout():
+  logout_user()
+  return redirect(url_for('index'))
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
   user = {'username': 'Steven'}
   posts = [
@@ -17,13 +27,48 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-  return render_template('index.html', title='Home', user=user, posts=posts)
+  return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
   form = LoginForm()
   if form.validate_on_submit():
-    flash(f"Login requested for {form.username.data} remember_me={form.remember_me.data}")
+    user = User.query.filter_by(username=form.username.data).first()
+    if user is None or not user.check_password(form.password.data):
+      flash('Invalid username or password')
+      return redirect(url_for('login'))
+    login_user(user, remember=form.remember_me.data)
+    next_page = request.args.get('next')
+    if not next_page or url_parse(next_page).netloc != '':
+      next_page = url_for('index')
     return redirect(url_for('index'))
   return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    user = User(username=form.username.data, email=form.email.data)
+    user.set_password(form.password.data)
+    db.session.add(user)
+    db.session.commit()
+    flash('Congratulations, you are now a registered user!')
+    return redirect(url_for('login'))
+  return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+  user = User.query.filter_by(username=username).first_or_404()
+  posts = [
+    {'author': user, 'body': 'Test post #1'},
+    {'author': user, 'body': 'Test post #2'}
+  ]
+  return render_template('user.html', user=user, posts=posts)
